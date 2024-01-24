6342858c477e6f1e4f49addea08342231282c4fc
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-01-23 21:24:14 -0500

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    x = 10
+    while (x > 0):
+        x -= 1
+        print(x)
+        if x == 1:
+            print("Happy New Year!")
+            break
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    x = [i ** 2 for i in int_list]
+    print(x)
+    return x
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(101):
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+        elif (i % 3 == 0):
+            print("Fizz")
+        elif (i % 5 == 0):
+            print("Buzz")
+        else:
+            print(i)
